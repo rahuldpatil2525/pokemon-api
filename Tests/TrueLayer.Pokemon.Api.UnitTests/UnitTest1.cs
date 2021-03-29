31a78b16e3d51dff307354b93725bf4049c7f7e3
rahuldpatil2525 <rahuldpatil2525@gmail.com>
2021-03-29 21:30:27 +0100

    Add basic solution

---
+++ Tests/TrueLayer.Pokemon.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TrueLayer.Pokemon.Api.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
